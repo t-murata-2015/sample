6899b415e4835c835c66831ce37e3a567e93d3bc
t_murata13 <murata.taijiro2015@gmail.com>
2020-04-22 00:42:11 +0900

    州別の値を取得

--- practiceArray.php
+++ practiceArray.php
@@ -32,38 +32,33 @@ asort($populationData);
 foreach($populationData as $key => $value){
     
     print $key.": ".$value."<br>";
-    $totalValue += $value;
     
 }
 print "<br>";
+
 
 
 print "3.<br>";
 $i = 0;
 $stateData = array();
 
+
 foreach($populationData as $key => $value){
     $key = substr($key, -2, 2);
-    print $key.": ".$value."<br>";
-    $stateData += array($i => array($key, $value));
-       ++$i; 
+   $stateData[$i] = $key;
+           ++$i; 
 }
-
-var_dump($stateData);
+$stateData2=array_unique($stateData);
 
 $j=0;
-foreach($stateData as $key => $value){
-    var_dump($stateData[$key][0]);
-    if($key != 0){
+foreach($stateData2 as $key1 => $value1){
+    foreach($populationData as $key2 => $value2){
+        $key3 = substr($key2, -2, 2);
+        if($value1==$key3){
+            $statePop[$j] += $value2;
+        }
+    }
 
-      if ($stateData[$key][0] == $stateData[$key-1][0]){
-        // $value[$j+1][1] += $value[$j][1];
-print "同じ";
-      }
-} 
-//print $value[$j][0].$value[$j][1];
-++$j; 
- }
-
-
-print "total: ".$totalValue."<br><br>";
+    print $value1.": ".$statePop[$j]."<br>";
+        ++$j;
+}
