e25ee3e806cb957f7c479e8b1c7dac1248776339
taijiro murata <taijiro.murata@onlineconsultant.jp>
2020-10-20 12:22:59 +0900

    cronのテスト

--- cron_test.php
+++ cron_test.php
@@ -1,18 +1,19 @@
 <?php
 
-date_default_timezone_set('Asia/Tokyo');
+$url = '/home/user/public_html/murata/';
+//$url = 'C:/xampp2/htdocs/samples/';
 
 // 昨日つくったファイルは消す
-$last_file_date = date("Y/m/d", strtotime('-1 day'));
-$last_file_name = $last_file_date . "_cron_test.txt";
+$last_file_date = date("Ymd",strtotime("-1 day"));;
+$last_file_name = $url.$last_file_date . "_cron_test.txt";
 
-if(file_exists($last_file_name)) { 
-    unlink($last_file_name);
+if(file_exists($last_file_name)) {
+	unlink($last_file_name);
 }
 
 // 新規で作成
-$new_file_date = date("Y/m/d");
-$new_file_name = $new_file_date . "_cron_test.txt";
+$new_file_date = date("Ymd");
+$new_file_name = $url . $new_file_date . "_cron_test.txt";
 touch($new_file_name);
 
 ?>
